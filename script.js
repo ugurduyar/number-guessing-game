bdf9291501ecc9789cf80fbf5c246b7a1b1d32f0
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-12 01:58:40 +0300

    constructors added for query selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const output = document.querySelector(".output");
+const message = document.querySelector(".message");
+const guessInput = document.querySelector("input");
+const btn = document.querySelector("button");
